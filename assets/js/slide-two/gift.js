cb184e621b4131a84f7188d62f8b202a6f76ac40
ntdat1306 <ntdat1306@gmail.com>
2023-02-27 01:06:24 +0700

    27022023

---
+++ assets/js/slide-two/gift.js
@@ -0,0 +1,21 @@
+$(function () {
+    function randomNum(m, n) {
+        m = parseInt(m)
+        n = parseInt(n)
+        return Math.floor(Math.random() * (n - m + 1)) + m
+    }
+
+    var clap = $('.clap')
+    var confetti = $('.confetti-effect')
+
+    clap.on('click', function () {
+        confetti.css('transform', 'rotate(' + randomNum(0, 180) + 'deg)')
+
+        confetti.children('.confetti-wrap').stop().addClass('expand')
+        setTimeout(function () {
+            confetti.children('.confetti-wrap').removeClass('expand')
+        }, 700)
+
+        $("#text-box").fadeIn("slow");
+    })
+})
